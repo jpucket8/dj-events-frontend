1640465be600123ba96d2cbcce998c7927e10640
JP <jpucket8@gmail.com>
2021-10-30 05:49:53 -0400

    add logic to display info on single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,54 @@
+import Link from "next/link";
+import Image from "next/image";
 import Layout from "@components/layout";
 import { API_URL } from "@config/index";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+
+import styles from "@styles/event.module.css";
 
 export default function EventPage({ evt }) {
+  const deleteEvent = e => {
+    e.preventDefault();
+    console.log("delete");
+  };
+
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+
+        <h1>{evt.name}</h1>
+
+        {evt.image && (
+          <div className={styles.image}>
+            <Image alt={evt.name} src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
