f1ddb5e08fb55855b95973842785f163b5d57ef3
JP <jpucket8@gmail.com>
2021-11-01 10:43:03 -0400

    store jwt in server http only cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+import cookie from "cookie";
 import { API_URL } from "@config/index";
 
 export default async (req, res) => {
@@ -19,7 +20,18 @@ export default async (req, res) => {
     const data = await strapiRes.json();
 
     if (strapiRes.ok) {
-      // @todo - Set cookie
+      // Set Cookie
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7,
+          sameSite: "strict",
+          path: "/",
+        })
+      );
+
       res.status(200).json({ user: data.user });
     } else {
       res
